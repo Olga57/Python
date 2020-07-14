e74c3bcc40eb03345c5dce67cd52d8cb39bf6e91
Olga57 <olga_subbotkina@mail.ru>
2020-07-14 22:23:28 +0300

    выполнила первое задание

---
+++ Homework-1.py
@@ -0,0 +1,22 @@
+#Задание 1
+name = 'Анна'
+period = '10'
+number = '5,3'
+result= 'True'
+report = 'None'
+
+print(name)
+print(period)
+print(number)
+print(result)
+print(report)
+
+
+name = str(input('Введите Ваше имя: '))
+print(name)
+
+age = int(input('Сколько Вам лет? '))
+print(age)
+
+city = str(input('В каком городе Вы живете? '))
+print(city)
