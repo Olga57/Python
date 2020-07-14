d6cb9119aa3682de8d09709ae38c2c94278dca07
Olga57 <olga_subbotkina@mail.ru>
2020-07-15 00:18:54 +0300

    выполнены задания 2,3,4

--- Homework-1.py
+++ Homework-1.py
@@ -27,3 +27,21 @@ hour=((second//3600))%24
 minute=(second//60)%60
 second=second%60
 print('{0}:{1:02}:{2:02}'.format(hour,minute,second))
+
+#Задание 3
+n=int(input('Введите n: '))
+num=str(n)
+n1=num+num
+n2=num+num+num
+total=n+int(n1)+int(n2)
+print('Результат равен: ',total)
+
+#Задание 4
+a=int(input('Введите целое положительное число: '))
+m=a%10
+a=a//10
+while a>10:
+    if a%10>m:
+        m=a%10
+    a=a//10
+print(m)
