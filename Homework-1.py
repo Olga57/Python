5fcb799f327cb3f9f03d1fc74b4f471a55412400
Olga57 <olga_subbotkina@mail.ru>
2020-07-14 23:19:32 +0300

    выполнено 2е задание

--- Homework-1.py
+++ Homework-1.py
@@ -20,3 +20,10 @@ print(age)
 
 city = str(input('В каком городе Вы живете? '))
 print(city)
+
+#Задание 2
+second=int(input('Введите секунды: '))
+hour=((second//3600))%24
+minute=(second//60)%60
+second=second%60
+print('{0}:{1:02}:{2:02}'.format(hour,minute,second))
