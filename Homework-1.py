a0a45b9a8550222b074f15f469802277b27516a6
Olga57 <olga_subbotkina@mail.ru>
2020-07-15 14:44:20 +0300

    выполнила все задания

--- Homework-1.py
+++ Homework-1.py
@@ -45,3 +45,34 @@ while a>10:
         m=a%10
     a=a//10
 print(m)
+
+#Задание 5
+revenue=float(input('Введите выручку фирмы: '))
+costs=float(input('Введите издержки фирмы: '))
+profit=revenue-costs
+if revenue>costs:
+    print(f'Фирма работает с прибылью. Рентабельность выручки составила {profit/costs: .2f}')
+    number_staff=int(input('Введите количество сотрудников фирмы: '))
+    print(f'Прибыль на одного сотрудника составила {profit/number_staff:.2f}')
+elif revenue==costs:
+    print('Фирма работает в ноль')
+else:
+    print('Фирма работает с убытком')
+
+#Задание 6
+a=int(input('Введите результаты пробежки первого дня в км: '))
+b=int(input('Введите общий желаемый результат в км: ' ))
+i=0
+while a<=b:
+    print(f'{i}-ый день:{a:2f}')
+    a=a+(a*0.1)
+    i=i+1
+else:
+    print(f'{i}-ый день:{a:2f}')
+    print(f'Ответ:на {i}-ый день спортсмен достиг результата - не менее {b}км.')
+
+
+
+
+
+
